4420ec59c674b3f1a9d387fcdbd2d29e1f80364d
f.janmohammadi <farshadjanmohammadi@gmail.com>
2024-11-15 22:29:55 +0330

    new Syntax react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
+import Products from "./pages/Products";
+import Location from "./pages/Location";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    children: [
+      {
+        path: "location",
+        element: <Location />,
+      },
+      {
+        index: true,
+        element: <About />,
+      },
+    ],
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "*",
+    element: <NoPage />,
+  },
+]);
 
 const App = () => {
-  return (
-    //classic syntax
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
