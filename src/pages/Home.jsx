57340956c7be17267284a09fc44eb11c53d66a54
f.janmohammadi <farshadjanmohammadi@gmail.com>
2024-11-16 21:26:12 +0330

    navigate programly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const [count, setCount] = useState(0);
+
+  const navigate = useNavigate();
 
   // const handleClick = () => {
   //   setCount((prev) => prev + 1);
@@ -10,10 +12,24 @@ const Home = () => {
   return (
     <div className="flex flex-col">
       <header className="flex gap-4">
-        <Link to="/">Home</Link>
+        {/* <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
-        <Link to="Products">Products</Link>
+        <Link to="Products">Products</Link> */}
+        <button
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Heme
+        </button>
+        <button
+          onClick={() => {
+            navigate("/about");
+          }}
+        >
+          about
+        </button>
       </header>
 
       {/* <header className="flex gap-4">
